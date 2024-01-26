630ffabccbb2722456cad8f59c70dccac3f5bb4e
Jb Ketele <149502977+jbketele@users.noreply.github.com>
2024-01-26 14:53:12 +0100

    conv html php

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-MZH9X7TZ0V"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-MZH9X7TZ0V');
+    </script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jean-Baptiste Kétèle</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="icon" type="image/x-icon" href="imgs/my-logo-fr.png">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.min.js">
+    <link rel="stylesheet" href="styles.css">
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            border: 0;
+            outline: 0;
+            font-size: 100%;
+            vertical-align: baseline;
+            background: transparent;
+            box-sizing: border-box;
+        }
+
+        html {
+            line-height: 1.15;
+            /* 1 /
+  -webkit-text-size-adjust: 100%; / 2 */
+            padding: 0;
+            margin: 0;
+        }
+
+        body {
+            background-image: url(imgs/AdobeStock_202119788.jpeg);
+            background-size: 100vw 100vh;
+            backdrop-filter: brightness(0.5);
+        }
+    </style>
+</head>
+
+<body>
+
+<?php require_once(__DIR__ . '/header.php'); ?>
+
+    <div class="landing">
+        <h1 class="display-3  d-flex justify-content-center text-white">BIENVENUE</h1>
+        <h2 class="name d-flex justify-content-center text-white">Jean-Baptiste Kétèle
+        </h2>
+        <p class="accroche d-flex justify-content-center text-white fs-4 fst-italic">En reconversion
+            professionnelle, de
+            l'agriculture
+            au
+            web</p>
+    </div>
+
+</body>
+
+</html
--- realisations.php
+++ realisations.php
@@ -57,42 +57,8 @@
 
 <body>
     <section>
-        <header class="menu d-flex">
-            <a href="index.html">
-                <img src="imgs/my-logo-fr_.png" class="logo mt-3" alt="">
-            </a>
-            <nav class="navbar navbar-expand-sm">
-                <div class="container">
-                    <ul class="navbar-nav">
-                        <li class="nav-item mt-1">
-                            <a class="navbar-brand text-dark" href="index.html">Accueil</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active text-dark" href="historique.html">Historique</a>
-                        <li class="nav-item">
-                            <a class="nav-link active text-dark" href="realisations.html">Réalisations</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active text-dark" href="propos.html">À propos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-dark" href="contact.html">Contact</a>
-                        </li>
-                    </ul>
-                    <div class="social">
-                        <a href="https://www.facebook.com/jean.bapt.ketele" target="_blank"><i class="bi bi-facebook"
-                                style="font-size: 2rem;"></i></a>
+    <?php require_once(__DIR__ . '/header.php'); ?>
 
-                        <a href="http://instagram.com/jbaptiste.ktl" target="_blank"><i class="bi bi-instagram"
-                                style="font-size: 2rem; color: #7232BD;"></i></a>
-                        <a href="https://www.youtube.com/channel/UCkGqOFKJUpH4UWUP80w8eTQ" target="_blank"><i
-                                class="bi bi-youtube" style="font-size: 2rem; color: red;"></i></a>
-                        <a href="https://www.linkedin.com/in/jean-baptiste-k%C3%A9t%C3%A8le-46673b233/"
-                            target="_blank"><i class="bi bi-linkedin" style="font-size: 2rem;"></i></a>
-                    </div>
-                </div>
-            </nav>
-        </header>
     </section>
     <h3 class="text-center text-white" style="padding-top: 2vh;padding-bottom: 2vh; backdrop-filter: brightness(0.5)">
         RÉALISATIONS</h3>
